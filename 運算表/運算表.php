9b72c894ee4173421e8ebbb181fb368294a6223c
adweei <apple871108@gmail.com>
2021-07-08 14:07:30 +0800

    運算表

---
+++ 運算表/運算表.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />	
+	<style type="text/css">
+	table,tr,td
+	{
+		padding: 0;
+	}
+	
+	table
+	{
+		border-width: medium;
+		width: 100%;
+		margin: 0 auto;
+	}
+	
+	table tr
+	{
+		line-height: 40px;
+	}
+	
+	table td
+	{
+		text-align: center;
+		border-color: yellow;
+	}
+	
+	table tr:nth-child(odd)
+	{
+		background: #00FFFF;
+	}
+
+	table tr:nth-child(even)
+	{
+		background: #7FFF00;
+	}
+
+</style>
+<body>
+
+<table border="1"> 
+<br />
+<b>Notice</b>:  Undefined index: num1 in <b>C:\xampp\htdocs\dirs\D1ACS106147\file\ex2\ACS106147.php</b> on line <b>45</b><br />
+<br />
+<b>Notice</b>:  Undefined index: num2 in <b>C:\xampp\htdocs\dirs\D1ACS106147\file\ex2\ACS106147.php</b> on line <b>46</b><br />
+<br />
+<b>Notice</b>:  Undefined index: num3 in <b>C:\xampp\htdocs\dirs\D1ACS106147\file\ex2\ACS106147.php</b> on line <b>47</b><br />
+<br />
+<b>Notice</b>:  Undefined index: num4 in <b>C:\xampp\htdocs\dirs\D1ACS106147\file\ex2\ACS106147.php</b> on line <b>48</b><br />
+<br />
+<b>Notice</b>:  Undefined index: op in <b>C:\xampp\htdocs\dirs\D1ACS106147\file\ex2\ACS106147.php</b> on line <b>49</b><br />
+<td><b></b></td></tr><tr><td><b></b></td><td></td></tr>
+
+</body>
+</html>
